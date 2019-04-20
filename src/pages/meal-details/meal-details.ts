17f9b79f96675b0a86efc89f696c8f37afa45e96
Benjemaa <scorpion.amor1@gmail.com>
2019-04-20 15:50:52 +0100

    added meal details
    Added meal Details PAge with some refractions

---
+++ src/pages/meal-details/meal-details.ts
@@ -0,0 +1,23 @@
+import { Component ,Input } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-meal-details',
+  templateUrl: 'meal-details.html',
+})
+export class MealDetailsPage {
+
+  @Input() meal: any;
+
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams) {
+    this.meal = navParams.get('meal');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MealDetailsPage');
+  }
+
+}
