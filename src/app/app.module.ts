6e6eefbbd330443f600e76c4c464d3897ba3db37
Hamza ben romdhane <hamzabnromdhane@gmail.com>
2019-04-21 23:20:56 +0100

    init notification
    get user token and send to api
    receive notification from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,31 +12,59 @@ import { MealsProvider } from '../providers/meals/meals';
 import { SpacesProvider } from '../providers/spaces/spaces';
 import { CommandProvider } from '../providers/command/command';
 
+// *********FIREBASE STUFF *********************
+import { Firebase } from '@ionic-native/firebase';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FcmProvider } from '../providers/fcm/fcm';
+
+const config = {
+	apiKey: "AIzaSyCwlbg0JdKXJFlLHIyDZaS9mS3JkQvUWpI",
+	authDomain: "wakelni-bc31e.firebaseapp.com",
+	databaseURL: "https://wakelni-bc31e.firebaseio.com",
+	projectId: "wakelni-bc31e",
+	storageBucket: "wakelni-bc31e.appspot.com",
+	messagingSenderId: "839423177030"
+}
+// ***************************************************
+
 @NgModule({
-  declarations: [
+	declarations: [
 		MyApp
-  ],
-  imports: [
+	],
+	imports: [
 		BrowserModule,
 		HttpClientModule,
 		IonicModule.forRoot(MyApp),
-		IonicStorageModule.forRoot()
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {
-      provide: ErrorHandler,
-      useClass: IonicErrorHandler
-    },
-    AuthService,
-    MealsProvider,
-    SpacesProvider,
-    CommandProvider
-  ]
+		IonicStorageModule.forRoot(),
+
+		// *********FIREBASE STUFF *********************
+		AngularFireModule.initializeApp(config), 
+    AngularFirestoreModule,
+		// ***************************************************
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp
+	],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{
+			provide: ErrorHandler,
+			useClass: IonicErrorHandler
+		},
+		AuthService,
+		MealsProvider,
+		SpacesProvider,
+		CommandProvider,
+    FcmProvider,
+
+		// *********FIREBASE STUFF *********************
+		Firebase,
+    FcmProvider
+		// ***************************************************
+	]
 })
-export class AppModule {}
+export class AppModule { }
